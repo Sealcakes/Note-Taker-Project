5c39227b0e012b9d2d483e1b0f54658fe4b4ad24
Sealcakes <taylorcseal@gmail.com>
2023-02-18 14:43:32 -0600

    adds helpers folder with .js files that create fs functions for reading/writing file data from and to the page

--- routes/notes.js
+++ routes/notes.js
@@ -1,22 +1,31 @@
 const notes = require('express').Router();
-const fs = require('fs');
-const path = require('path');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
 
 
 notes.get('/', (req,res) => {
     console.info(`${req.method} request received for notes`);
-    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err)
-        } else {
-            res.send(data);
-        }
-    })
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 notes.post('/', (req,res) => {
     console.info(`${req.method} request received for notes`);
     
+    const { title, text } = req.body;
+
+    if(req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid()
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Tip added successfully`);
+    } else {
+        res.error('Error in adding tip');
+    }
 });
 
 notes.delete('/', (req, res) => {
