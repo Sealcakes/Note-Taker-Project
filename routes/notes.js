cc02d11897e2a296c08c03e4c0b3208b9c307173
Sealcakes <taylorcseal@gmail.com>
2023-02-18 14:07:18 -0600

    adds modular routes for notes page and adds notes.get response to show db.json data on page

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,26 @@
 const notes = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+
 
 notes.get('/', (req,res) => {
+    console.info(`${req.method} request received for notes`);
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err)
+        } else {
+            res.send(data);
+        }
+    })
+});
+
+notes.post('/', (req,res) => {
+    console.info(`${req.method} request received for notes`);
     
-})
+});
+
+notes.delete('/', (req, res) => {
+    console.info(`${req.method} request received for notes`)
+});
+
+module.exports = notes;
