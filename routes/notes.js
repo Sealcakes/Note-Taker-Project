d60ff03159de4ba985f011b3527eedc41c015021
Sealcakes <taylorcseal@gmail.com>
2023-02-21 19:28:56 -0600

    tried to get code to work, made changes to notes.js file

--- routes/notes.js
+++ routes/notes.js
@@ -1,35 +1,80 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
+const fs = require('fs');
+const uniqid = require('uniqid');
+const util = require('util');
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 notes.get('/', (req,res) => {
     console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    
+    readFileAsync('./db/db.json', 'utf8').then((data) => {
+        note = [].concat(JSON.parse(data))
+        res.json(note);
+    })
+
+    // let data = fs.readFileSync('./db/db.json', 'utf8');
+
+    // res.json(JSON.parse(data));
 });
 
 notes.post('/', (req,res) => {
     console.info(`${req.method} request received for notes`);
     
-    const { title, text } = req.body;
+    const newNote = req.body;
+    readFileAsync('./db/db.json', 'utf8').then((data) => {
+        const note = [].concat(JSON.parse(data));
+        note.id = uniqid();
+        note.push(newNote);
+        return note;
+    }).then((note) => {
+        writeFileAsync('./db/db.json', JSON.stringify(note))
+        res.json(note);
+    })
 
-    if(req.body) {
-        const newNote = {
-            title,
-            text,
-            note_id: uuid()
-        };
 
-        readAndAppend(newNote, './db/db.json');
-        res.json(`Tip added successfully`);
-    } else {
-        res.error('Error in adding tip');
-    }
+
+    // const newNote = {
+    //     ...req.body,
+    //     note_id: uniqid()
+    // };  
+    
+
+    // let data = fs.readFileSync('./db/db.json', 'utf-8');
+
+    // const parsedData = JSON.parse(data);
+
+    // parsedData.push(newNote);
+
+    // fs.writeFile('./db/db.json', JSON.stringify(parsedData), (err, text) => {
+    //     if (err) {
+    //         console.error(err)
+    //         return;
+    //     }
+    //     console.log('Test', text);
+    // })
+
+    // res.json(data);
 });
 
-notes.delete('/', (req, res) => {
-    console.info(`${req.method} request received for notes`)
+notes.delete('/:id', (req, res) => {
+    const noteToDelete = req.params.id;
+    console.log(noteToDelete);
+    readFileAsync('./db/db.json', 'utf-8').then((data) => {
+        const note = [].concat(JSON.parse(data));
+        const newNotesData = [];
+        for (let i = 0; i < note.length; i++) {
+            if (noteToDelete !== note[i].id) {
+                newNotesData.push(note[i])
+            }
+        }
+        return newNotesData
+    }).then((note) => {
+        writeFileAsync('./db/db.json', JSON.stringify(note))
+        res.send('Note saved successfully');
+    })
 });
 
 module.exports = notes;
